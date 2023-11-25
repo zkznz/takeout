fa2e5b08dfe2ce6609a912c558bf44e7535b5064
lzk <1174633615@qq.com>
2023-11-25 10:36:01 +0800

    完成根据条件查询分类数据接口

--- src/main/java/com/example/reggie_takeout/controller/CategoryController.java
+++ src/main/java/com/example/reggie_takeout/controller/CategoryController.java
@@ -6,10 +6,12 @@ import com.example.reggie_takeout.common.R;
 import com.example.reggie_takeout.entity.Category;
 import com.example.reggie_takeout.entity.Employee;
 import com.example.reggie_takeout.service.CategoryService;
+import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -47,5 +49,16 @@ public class CategoryController{
         category.setUpdateUser(empId);
         categoryService.updateById(category);
         return R.success("修改分类信息成功");
+    }
+    /*
+    根据条件查询分类数据
+    * */
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        LambdaQueryWrapper<Category> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
+        queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+        List<Category> list= categoryService.list(queryWrapper);
+        return R.success(list);
     }
 }
